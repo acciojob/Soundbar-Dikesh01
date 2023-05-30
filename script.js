c223ce118f12e3813d4a7417289973405f26266b
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-30 07:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+let audios = [
+	"applause",
+	"boo",
+	"gasp",
+	"tada",
+	"victory",
+	"wrong",
+	"stop"
+]
+
+let activeAudio = null;
+
+const button = document.getElementsByTagName("button");
+
+function playAudio(){
+    stopAudio();
+	activeAudio.play();
+}
+
+function stopAudio(){
+	if(activeAudio){
+		activeAudio.pause();
+    // activeAudio = null;
+	}
+}
+
+for(let i=0; i<button.length; i++){
+	button[i].addEventListener("click", (event) =>{
+		let innerText = event.target.innerText;
+    
+		if(audios.includes(innerText)){
+
+			let audioElement = document.createElement("audio");
+			audioElement.src = `https://samplelib.com/lib/preview/mp3/sample-12s.mp3`;
+			audioElement.controls = true;
+			activeAudio = audioElement;
+			playAudio();
+		}
+    
+    else{
+      stopAudio();
+    }
+	})
+}
